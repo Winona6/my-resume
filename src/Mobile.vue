0e7f7f5baa85c5f8b2495a9db10c5b08158e11b5
winona6 <wwy0527@163.com>
2019-10-30 13:36:48 +0800

    winona resume commit

---
+++ src/Mobile.vue
@@ -0,0 +1,270 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+import StyleEditor from "./components/StyleEditor";
+import ResumeEditor from "./components/ResumeEditor";
+import "./assets/reset.css";
+
+export default {
+  name: "app",
+  components: {
+    StyleEditor,
+    ResumeEditor
+  },
+  data() {
+    return {
+      interval: 40,
+      currentStyle: "",
+      enableHtml: false,
+      fullStyle: [
+        `/*
+* 大家好，我是魏婉莹。
+* 前几日无意之间在网上看到一位大神做的动态简历。 
+* 哇，我觉得很酷哦。
+* 说做就做，我也来写一份简历！*/
+
+/* 首先给所有元素加上过渡效果 */
+* {
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222);
+  background: rgb(0,43,54);
+}
+/* 文字离边框太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  overflow: auto;
+  width: 90vw;
+  margin: 2.5vh 5vw;
+  height: 90vh;
+}
+/* 太高了 */
+.styleEditor {
+  height: 45vh;
+}
+/* 代码高亮 */
+.token.selector{
+  color: rgb(133,153,0);
+}
+.token.property{
+  color: rgb(187,137,0);
+}
+.token.punctuation{
+  color: yellow;
+}
+.token.function{
+  color: rgb(42,161,152);
+}
+
+/* 加点 3D 效果呗 */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  transform: rotateX(-10deg) translateZ(-50px) ;
+}
+
+/* 接下来我给自己准备一个编辑器 */
+.resumeEditor{
+  position: fixed;
+  top: 50%; left: 0;
+  padding: .5em;  margin: 2.5vh;
+  width: 95vw; height: 45vh;
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 好了，我开始写简历了 */
+
+
+`,
+        `
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
+`,
+        `
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`
+      ],
+      currentMarkdown: "",
+      fullMarkdown: `魏婉莹
+--------
+一名前端路上不断前进的小白~ 
+基本信息
+--------
+* 性别：女
+* 年龄：22
+* 邮箱：wwy0527@163.com
+* 电话：13576951363
+
+教育背景
+--------
+* 2015.09—2019.06 东华理工大学软件学院 本科    
+* 主修专业：软件工程
+* 主修课程：web 程序设计、移动互联网应用开发、智能手机软件开发、HTML5 应用、数据库、 Linux 操作系统、数据结构与算法基础、XML 基础、人机交互与界面设计等
+
+职业技能
+--------
+* 掌握 html5、CSS3、JS、Ajax、JSON 等前端开发技术
+* 熟练完成产品视觉设计与数据交互
+* 熟悉响应式 web 设计工作原理，实现响应式布局
+* 了解 MVVM 设计模式，使用过 Vue.js、Angular.js 等前端主流框架
+* 了解微信公众号和小程序开发
+* 使用过 Webpack 打包工具以及 sass 等 css 工具
+* 对 Node.js、数据库等后端技术有所了解及应用
+
+学生工作
+--------
+* 班级团支部书记
+* 党支部微信公众号运营小组
+* 校新媒体广兰之声中文播音部播音员
+* 院学生会组织部干事  
+
+获奖情况
+--------
+* 校主持人大赛三等奖
+* “青春杯”院演讲比赛一等奖   
+* 荣获“优秀学生干部”称号                        
+* 江西省计算机作品大赛荣获“先进个人”称号 
+* 校二等奖学金              
+`
+    };
+  },
+  created() {
+    this.makeResume();
+  },
+
+  methods: {
+    makeResume: async function() {
+      await this.progressivelyShowStyle(0);
+      await this.progressivelyShowResume();
+      await this.progressivelyShowStyle(1);
+      await this.showHtml();
+      await this.progressivelyShowStyle(2);
+    },
+    showHtml: function() {
+      return new Promise((resolve, reject) => {
+        this.enableHtml = true;
+        this.$nextTick(() => {
+          this.$refs.resumeEditor.goTop();
+        });
+        resolve();
+      });
+    },
+    progressivelyShowStyle(n) {
+      return new Promise((resolve, reject) => {
+        let interval = this.interval;
+        let showStyle = async function() {
+          let style = this.fullStyle[n];
+          if (!style) {
+            return;
+          }
+          // 计算前 n 个 style 的字符总数
+          let length = this.fullStyle
+            .filter((_, index) => index <= n)
+            .map(item => item.length)
+            .reduce((p, c) => p + c, 0);
+          let prefixLength = length - style.length;
+          if (this.currentStyle.length < length) {
+            let l = this.currentStyle.length - prefixLength;
+            let char = style.substring(l, l + 1) || " ";
+            this.currentStyle += char;
+            if (style.substring(l - 1, l) === "\n" && this.$refs.styleEditor) {
+              this.$nextTick(() => {
+                this.$refs.styleEditor.goBottom();
+              });
+            }
+            setTimeout(showStyle, interval);
+          } else {
+            resolve();
+          }
+        }.bind(this);
+        showStyle();
+      });
+    },
+    progressivelyShowResume() {
+      return new Promise((resolve, reject) => {
+        let length = this.fullMarkdown.length;
+        let interval = this.interval;
+        let showResume = () => {
+          if (this.currentMarkdown.length < length) {
+            this.currentMarkdown = this.fullMarkdown.substring(
+              0,
+              this.currentMarkdown.length + 1
+            );
+            let lastChar = this.currentMarkdown[
+              this.currentMarkdown.length - 1
+            ];
+            let prevChar = this.currentMarkdown[
+              this.currentMarkdown.length - 2
+            ];
+            if (prevChar === "\n" && this.$refs.resumeEditor) {
+              this.$nextTick(() => this.$refs.resumeEditor.goBottom());
+            }
+            setTimeout(showResume, interval);
+          } else {
+            resolve();
+          }
+        };
+        showResume();
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  position: relative;
+}
+
+html {
+  min-height: 100vh;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
